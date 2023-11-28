7df3c1566a2e8d50eec03588878e9b250390b6bd
Guilherme Fonseca <guilhermao1234@hotmail.com>
2023-11-27 23:17:19 -0300

    feat: add structure of routes and home page

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,14 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import Start from "../Pages/Start"
 import AppExplanation from "../Pages/AppExplanation";
 import Home from "../Pages/Home";
 import HabitPage from "../Pages/HabitPage";
 
-const Stack = createNativeStackNavigator();
 
-export default function AllPages() {
+const stack = createNativeStackNavigator();
+
+export default function HomePage() {
     return (
         <NavigationContainer>
 
@@ -16,14 +15,12 @@ export default function AllPages() {
                 screenOptions={{
                     headerShown: false,
                 }} >
-
-                <Stack.Screen name="Start" component={Start} />
-                <Stack.Screen name="AppExplanation" component={AppExplanation} />
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Home" component={HabitPage} />
+                <Stack.Screen name="AppExplanation" component={AppExplanation} />
 
             </Stack.Navigator>
 
         </NavigationContainer>
     )
-}
+} 
